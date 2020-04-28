89285473d8c6ebc0aee9ff46eca1117bd6cf9c83
Marion Stadtmüller <marion.stadtmueller@gmail.com>
2020-04-28 10:53:45 +0200

    EULE hinzugefügt

---
+++ EULE.java
@@ -0,0 +1,21 @@
+public class EULE implements Audio
+{
+    int alter;
+    int gewicht;
+    
+    EULE()
+    {
+        gewicht = 2;
+        alter = 20;
+    }
+    
+    EULE(int alterNeu, int gewichtNeu)
+    {
+        gewicht = gewichtNeu;
+        alter = alterNeu;
+    }
+    
+    public String lautGeben() {
+        return "SchuHu";
+    }
+}
