2264626f63f13b1bf9f24174d69e613dfa1c8430
Googolple3x <nikolaihiemer@gmx.de>
2020-04-28 11:22:47 +0200

    Update2

--- RIESENFAULTIER.java
+++ RIESENFAULTIER.java
@@ -1,6 +1,21 @@
-class RIESENFAULTIER
+class RIESENFAULTIER implements Audio
 {
+    int alter;
+    int gewicht;
+    
     RIESENFAULTIER()
     {
+        gewicht = 250;
+        alter = 6;
+    }
+    
+    RIESENFAULTIER(int alterNeu, int gewichtNeu)
+    {
+        gewicht = gewichtNeu;
+        alter = alterNeu;
+    }
+    
+    public String lautGeben() {
+        return "uuuuuuuuuuuuaaaaaa";
     }
 }
