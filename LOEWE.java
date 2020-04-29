2f59bf61ed143395feed583bc9a1b267b3b9fef3
Alina Ruggaber <alina.ruggaber@gmail.com>
2020-04-29 10:50:27 +0200

    loewe setter

--- LOEWE.java
+++ LOEWE.java
@@ -2,31 +2,40 @@ public class LOEWE implements Audio
 {
     int alter;
     int gewicht;
-    
+
     LOEWE()
     {
         gewicht = 100;
         alter = 3;
     }
-    
+
     LOEWE(int alterNeu, int gewichtNeu)
     {
         gewicht = gewichtNeu;
         alter = alterNeu;
     }
-    
+
     public String lautGeben() {
         return "RRRRRooooaaaRRRR";
     }
-    
+
     public int alterGeben()
     { 
         return alter;
     }
-    
+
     public int gewichtGeben()
     { 
-        return alter;
+        return gewicht;
     }
 
+    public void alterSetzen(int alterNeu)
+    {
+        alter = alterNeu;
+    }
+
+    public void gewichtSetzen(int gewichtNeu)
+    {
+        gewicht = gewichtNeu;
+    }
 }
